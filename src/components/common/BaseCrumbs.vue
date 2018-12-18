bf374f8d3e9e422cee61060613dc8f74f3a5a866
良方 <chenliangfang@diligrp.com>
2018-12-18 18:31:47 +0800

    temp

---
+++ src/components/common/BaseCrumbs.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="top-crumbs">
+    当前位置：{{pos}}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'BaseCrumbs',
+  props: {
+    pos: String
+  },
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.top-crumbs {
+  color: red;
+}
+</style>
---
+++ src/views/steve/steve.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <base-crumbs pos="结账明细"></base-crumbs>
+    steve hh
+  </div>
+</template>
+
+<script>
+import BaseCrumbs from '@/components/common/BaseCrumbs.vue'
+export default {
+  components: {
+    BaseCrumbs
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
